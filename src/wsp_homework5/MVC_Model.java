66b2d38fdbddf5174867d6bec237d621c5403718
htonyn-d <htonynguyen@gmail.com>
2017-11-09 01:48:46 -0500

    Added Model

---
+++ src/wsp_homework5/MVC_Model.java
@@ -0,0 +1,11 @@
+package wsp_homework5;
+
+import java.util.EventObject;
+
+public class MVC_Model extends EventObject {
+    
+    public MVC_Model(Object source) {
+        super(source);
+    }
+    
+}
